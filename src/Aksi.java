1de5f94647b6df709e4b78b15ae80e159be87772
Sa-adillah <lerox.3rd@gmail.com>
2021-06-23 19:17:14 +0800

    Membuat 2 object untuk setiap constructor

--- src/Aksi.java
+++ src/Aksi.java
@@ -1,6 +1,15 @@
+import java.util.Date;
+
 //class untuk menciptakan object
 public class Aksi {
     public static void main(String[] args) {
+        Karyawan sadil = new Karyawan();
+        Karyawan ani = new Karyawan();
+        Karyawan dillah = new Karyawan(2,"dillah","Laki-laki",new Date(),new Date(),"Administrasi","Banjarbaru","081545709220");
+        Karyawan susilawati = new Karyawan(3, "susilawati","Perempuan",new Date(),new Date(),"Marketing","Landasan Ulin","081545709230");
+        Karyawan agus = new Karyawan(4, "agus", "Laki Laki");
+        Karyawan wati = new Karyawan(5,"wati","Perempuan");
+
 
     }
 }
